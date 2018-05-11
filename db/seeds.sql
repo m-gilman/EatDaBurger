fdd3e74080a5e5ac10dcb76ab5d358e1845c5953
Melissa Gilman <melissadgilman@yahoo.com>
2018-05-11 15:54:17 -0600

    initial commit. Setup of file structure

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whiskey River BBQ Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avocado & Swiss Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pineapple Wasabi Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Boring Burger', false);
+
+SELECT * FROM burgers;
+
